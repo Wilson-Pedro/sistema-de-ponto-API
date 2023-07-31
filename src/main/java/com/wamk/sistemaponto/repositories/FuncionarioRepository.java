736ea6d1c67b4d50814f896c6a2d86b12792c647
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-07-31 16:56:52 -0300

    FuncionarioRepository e RegistroRepository

---
+++ src/main/java/com/wamk/sistemaponto/repositories/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+package com.wamk.sistemaponto.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wamk.sistemaponto.model.Funcionario;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
+
+}
---
+++ src/main/java/com/wamk/sistemaponto/repositories/RegistroRepository.java
@@ -0,0 +1,9 @@
+package com.wamk.sistemaponto.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wamk.sistemaponto.model.Registro;
+
+public interface RegistroRepository extends JpaRepository<Registro, Long>{
+
+}
