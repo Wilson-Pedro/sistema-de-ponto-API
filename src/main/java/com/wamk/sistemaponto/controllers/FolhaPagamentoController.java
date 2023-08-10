0f46499af500f513ff55931e1e80b5046d2f9a9b
Wilson-Pedro <wilsonpedroneto@gmail.com>
2023-08-10 19:10:04 -0300

    Métodos GETS na FolhaPagamento

--- src/main/java/com/wamk/sistemaponto/controllers/FolhaPagamentoController.java
+++ src/main/java/com/wamk/sistemaponto/controllers/FolhaPagamentoController.java
@@ -1,9 +1,16 @@
 package com.wamk.sistemaponto.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wamk.sistemaponto.model.FolhaPagamento;
 import com.wamk.sistemaponto.servcies.FolhaPagamentoService;
 
 @RestController
@@ -12,4 +19,16 @@ public class FolhaPagamentoController {
 	
 	@Autowired
 	private FolhaPagamentoService folhaPagamentoService;
+	
+	@GetMapping
+	public ResponseEntity<List<FolhaPagamento>> findAll(){
+		List<FolhaPagamento> list = folhaPagamentoService.findAll();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<FolhaPagamento> findById(@PathVariable Long id){
+		Optional<FolhaPagamento> folhaPagamentoOpt = folhaPagamentoService.findById(id);
+		return ResponseEntity.ok(folhaPagamentoOpt.get());
+	}
 }
--- src/main/java/com/wamk/sistemaponto/servcies/FolhaPagamentoService.java
+++ src/main/java/com/wamk/sistemaponto/servcies/FolhaPagamentoService.java
@@ -1,8 +1,12 @@
 package com.wamk.sistemaponto.servcies;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.wamk.sistemaponto.model.FolhaPagamento;
 import com.wamk.sistemaponto.repositories.FolhaPagamentoRepository;
 
 @Service
@@ -10,4 +14,12 @@ public class FolhaPagamentoService {
 
 	@Autowired
 	private FolhaPagamentoRepository folhaPagamentoRepository;
+
+	public List<FolhaPagamento> findAll() {
+		return folhaPagamentoRepository.findAll();
+	}
+
+	public Optional<FolhaPagamento> findById(Long id) {
+		return folhaPagamentoRepository.findById(id);
+	}
 }
